c0adab2c0115b0badc86996f77c560c0b1f92e40
Roy Anger <roy@royanger.com>
2022-08-24 23:30:14 -0400

    Added license, fixed first deploy errors

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,86 +1,86 @@
-import * as React from 'react';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import Link from 'next/link';
-import { useForm } from 'react-hook-form';
-import Header2 from '../../components/Header2';
-import { Input } from '../../components/input';
-import { Title } from '../../components/Title';
-import { Layout } from '../../components/ui/Layout';
+import * as React from 'react'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import Header2 from '../../components/Header2'
+import { Input } from '../../components/Input'
+import { Title } from '../../components/Title'
+import { Layout } from '../../components/ui/Layout'
 
 export default function Register() {
-   const [error, setError] = React.useState({ status: false, message: '' });
+   const [error, setError] = React.useState({ status: false, message: '' })
    const {
       register,
       handleSubmit,
       watch,
       formState: { errors },
-   } = useForm();
+   } = useForm()
 
-   const signinMutation = useMutation((data) => {
+   const signinMutation = useMutation(data => {
       const body = {
          username: data.username,
          password: data.password,
-      };
+      }
       return axios
          .post('/api/auth/register', data)
-         .then((res) => {})
-         .catch((err) => {
-            setError({ status: true, message: err.response.data.message });
-         });
-   });
+         .then(res => {})
+         .catch(err => {
+            setError({ status: true, message: err.response.data.message })
+         })
+   })
 
-   const onSubmit = (data) => {
-      signinMutation.mutate(data);
-   };
+   const onSubmit = data => {
+      signinMutation.mutate(data)
+   }
 
    return (
       <>
          <Header2 />
-         <Link href='/'>
-            <p className='flex flex-row mt-6'>
+         <Link href="/">
+            <p className="flex flex-row mt-6">
                <img
-                  src='/images/icons/back-arrow.svg'
-                  alt='Back'
-                  className='mr-2'
+                  src="/images/icons/back-arrow.svg"
+                  alt="Back"
+                  className="mr-2"
                />
                Back
             </p>
          </Link>
-         <div className='flex flex-col items-center mt-3'>
+         <div className="flex flex-col items-center mt-3">
             <img
-               className='w-36'
-               src='/images/illustrations/with-phone.svg'
-               alt='Girl on phone'
+               className="w-36"
+               src="/images/illustrations/with-phone.svg"
+               alt="Girl on phone"
             />
-            <Title variant='h1' className='mt-2'>
+            <Title variant="h1" className="mt-2">
                Create Account
             </Title>
          </div>
-         <Title variant='h2'>
+         <Title variant="h2">
             Join your fellow Terrarians - it takes less than a minute!
          </Title>
          <form onSubmit={handleSubmit(onSubmit)}>
             <Input
-               type='email'
-               id='email'
-               label='Email'
+               type="email"
+               id="email"
+               label="Email"
                required={true}
                register={register}
-               defaultValue='royanger@gmail.com'
+               defaultValue="royanger@gmail.com"
             />
             <Input
-               type='text'
-               id='name'
-               label='Name'
+               type="text"
+               id="name"
+               label="Name"
                required={true}
                register={register}
-               defaultValue='Roy A.'
+               defaultValue="Roy A."
             />
             <Input
-               type='password'
-               id='password'
-               label='Password'
+               type="password"
+               id="password"
+               label="Password"
                required={true}
                register={register}
             />
@@ -92,9 +92,9 @@ export default function Register() {
          <button>Continue with Google</button>
          <button>Continue with Facebook</button>
          <h5>Have an account?</h5>
-         <a href='/signin'>Sign In</a>
+         <a href="/signin">Sign In</a>
       </>
-   );
+   )
 }
 
 Register.getLayout = function getLayout(page) {
@@ -102,5 +102,5 @@ Register.getLayout = function getLayout(page) {
       <>
          <Layout>{page}</Layout>
       </>
-   );
-};
+   )
+}
--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,65 +1,64 @@
-import * as React from 'react';
-import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import Header2 from '../components/Header2';
-import { Title } from '../components/Title';
-import { Input } from '../components/input';
+import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import Header2 from '../components/Header2'
+import { Title } from '../components/Title'
+import { Input } from '../components/Input'
 
 export function SignIn() {
-   const [error, setError] = React.useState({});
-   const navigate = useNavigate();
+   const [error, setError] = React.useState({})
+   const navigate = useNavigate()
 
    const {
       register,
       handleSubmit,
       formState: { errors },
-   } = useForm();
+   } = useForm()
 
-   const loginMutation = useMutation((data) => {
+   const loginMutation = useMutation(data => {
       return axios
          .post('http://localhost:3002/auth/login', data)
-         .then((res) => {
-            navigate('/dashboard');
+         .then(res => {
+            navigate('/dashboard')
          })
-         .catch((err) => {
-            setError(err.response.data.error);
-         });
-   });
+         .catch(err => {
+            setError(err.response.data.error)
+         })
+   })
 
-   const onSubmit = async (data) => {
-      loginMutation.mutate(data);
-   };
+   const onSubmit = async data => {
+      loginMutation.mutate(data)
+   }
 
    return (
       <>
          <Header2 />
-         <Link to='/'>
-            <p className='flex flex-row mt-6'>
+         <Link to="/">
+            <p className="flex flex-row mt-6">
                <img
-                  src='/images/icons/back-arrow.svg'
-                  alt='Back'
-                  className='mr-2'
+                  src="/images/icons/back-arrow.svg"
+                  alt="Back"
+                  className="mr-2"
                />
                Back
             </p>
          </Link>
-         <div className='flex flex-col items-center mt-3'>
+         <div className="flex flex-col items-center mt-3">
             <img
-               className='w-36'
-               src='/images/illustrations/with-phone.svg'
-               alt='Girl on phone'
+               className="w-36"
+               src="/images/illustrations/with-phone.svg"
+               alt="Girl on phone"
             />
-            <Title variant='h1' className='mt-2'>
+            <Title variant="h1" className="mt-2">
                Sign In
             </Title>
          </div>
          <form onSubmit={onSubmit}>
             <Input
-               type='email'
-               id='username'
-               label='Email'
+               type="email"
+               id="username"
+               label="Email"
                required={true}
                register={register}
                customCSS={
@@ -68,9 +67,9 @@ export function SignIn() {
                }
             />
             <Input
-               type='password'
-               id='password'
-               label='Password'
+               type="password"
+               id="password"
+               label="Password"
                required={true}
                register={register}
                customCSS={
@@ -80,7 +79,7 @@ export function SignIn() {
             />
 
             <button>Sign In</button>
-            <div className='text-error w-full flex-col items-center'>
+            <div className="text-error w-full flex-col items-center">
                {error && <p>{error.message}</p>}
                {errors.username && <p>Please enter a valid email</p>}
                {errors.password && <p>Please enter a password</p>}
@@ -88,14 +87,14 @@ export function SignIn() {
          </form>
          <p>Forgot Password?</p>
          <p>or</p>
-         <a href='http://localhost:3002/auth/google'>
+         <a href="http://localhost:3002/auth/google">
             <button>Continue with Google</button>
          </a>
-         <a href='http://localhost:3002/auth/facebook'>
+         <a href="http://localhost:3002/auth/facebook">
             <button>Continue with Facebook</button>
          </a>
          <button>Join the Terrarians</button>
          <p>Don't have account?</p>
       </>
-   );
+   )
 }
